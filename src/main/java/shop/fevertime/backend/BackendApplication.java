174b757f313c0273c30bd9476bb8c0010c1612ff
gusrb8925 <gusrb8925@naver.com>
2021-11-26 17:19:56 +0900

    feat : application 중요정보 감추기 (#5)

--- src/main/java/shop/fevertime/backend/BackendApplication.java
+++ src/main/java/shop/fevertime/backend/BackendApplication.java
@@ -1,22 +1,15 @@
 package shop.fevertime.backend;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(BackendApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-//        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 //    @Bean
