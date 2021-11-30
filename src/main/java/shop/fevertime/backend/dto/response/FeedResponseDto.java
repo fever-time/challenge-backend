3a2158876283a246644acd09876ccb56f2480bb4
jackson <steady8@kakao.com>
2021-11-30 10:37:23 +0900

    fix: 피드 객체 반환 시 feedId, kakaoId 추가 반환

--- src/main/java/shop/fevertime/backend/dto/response/FeedResponseDto.java
+++ src/main/java/shop/fevertime/backend/dto/response/FeedResponseDto.java
@@ -12,14 +12,18 @@ import java.util.List;
 @Getter
 @Setter
 public class FeedResponseDto {
+    private Long feedId;
     private String contents;
     private String username;
     private List<CommentResponseDto> comments = new ArrayList<>();
     private LocalDateTime lastModifiedDate;
+    private Long kakaoId;
 
     public FeedResponseDto(Feed feed) {
+        this.feedId = feed.getId();
         this.contents = feed.getContents();
         this.username = feed.getUser().getUsername();
+        this.kakaoId = feed.getUser().getKakaoId();
         feed.getComments().stream()
                 .map(CommentResponseDto::new)
                 .forEach(commentResponseDto -> this.comments.add(commentResponseDto));
