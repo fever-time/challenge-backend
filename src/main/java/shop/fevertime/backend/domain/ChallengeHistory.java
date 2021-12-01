f352e9ba63e83bb7ee8a9f1c52c42e4e67580420
gusrb8925 <gusrb8925@naver.com>
2021-12-01 16:23:48 +0900

    fix : 챌린지 LocationType 추가

--- src/main/java/shop/fevertime/backend/domain/ChallengeHistory.java
+++ src/main/java/shop/fevertime/backend/domain/ChallengeHistory.java
@@ -5,15 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Report extends BaseTimeEntity {
+public class ChallengeHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "report_id")
+    @Column(name = "challenge_history_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -21,6 +22,16 @@ public class Report extends BaseTimeEntity {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "certification_id")
-    private Certification certification;
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(updatable = false)
+    private LocalDateTime missionDate;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private ChallengeStatus challengeStatus;
 }
