c7fc30d25043f8d41d37c31ae81a9f548c3c0c0c
jackson <steady8@kakao.com>
2021-12-08 00:17:56 +0900

    test : ChallengeHistoryRepositoryTest 테스트 케이스 생성 (#48)

--- src/test/java/shop/fevertime/backend/repository/ChallengeHistoryRepositoryTest.java
+++ src/test/java/shop/fevertime/backend/repository/ChallengeHistoryRepositoryTest.java
@@ -1,15 +1,18 @@
 package shop.fevertime.backend.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import shop.fevertime.backend.domain.*;
+import shop.fevertime.backend.exception.ApiRequestException;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,24 +53,47 @@ class ChallengeHistoryRepositoryTest {
     }
 
     @Test
+    @DisplayName("챌린지 기록 생성")
     @Order(1)
-    public void save_delete() {
+    public void save() {
         // given
         ChallengeHistory challengeHistory = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
 
         // when
         challengeHistoryRepository.save(challengeHistory);
 
-        challengeHistoryRepository.delete(challengeHistory);
+        //then
+        ChallengeHistory history = challengeHistoryRepository.findChallengeHistoryByChallengeStatusEquals(ChallengeStatus.JOIN, user1, challenge1).orElseThrow(
+                () -> new ApiRequestException("챌린지에 참여한 내역이 없습니다.")
+        );
+        assertThat(history.getChallengeStatus()).isEqualTo(ChallengeStatus.JOIN);
     }
 
     @Test
+    @DisplayName("챌린지 기록 삭제")
     @Order(2)
+    public void cancel() {
+        // given
+        ChallengeHistory challengeHistory = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+
+        // when
+        challengeHistoryRepository.save(challengeHistory);
+        challengeHistoryRepository.delete(challengeHistory);
+
+        //then
+        assertThat(challengeHistoryRepository.findChallengeHistoryByChallengeStatusEquals(ChallengeStatus.JOIN, user1, challenge1)).isEmpty();
+
+
+    }
+
+    @Test
+    @DisplayName("챌린지 몇 번 참여했는지 확인")
+    @Order(3)
     public void findAll() {
         // given
         ChallengeHistory challengeHistory1 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
-        ChallengeHistory challengeHistory2 = new ChallengeHistory(user1, challenge2, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
-        ChallengeHistory challengeHistory3 = new ChallengeHistory(user2, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory2 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory3 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
         ChallengeHistory challengeHistory4 = new ChallengeHistory(user2, challenge2, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
 
         challengeHistoryRepository.save(challengeHistory1);
@@ -76,9 +102,52 @@ class ChallengeHistoryRepositoryTest {
         challengeHistoryRepository.save(challengeHistory4);
 
         // when
-        List<ChallengeHistory> all = challengeHistoryRepository.findAll();
+        List<ChallengeHistory> all = challengeHistoryRepository.findAllByChallengeAndUser(challenge1, user1);
 
         // then
-        assertThat(all.size()).isEqualTo(4);
+        assertThat(all.size()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("챌린지 참여자 수")
+    @Order(4)
+    void count_participants() {
+        // given
+        ChallengeHistory challengeHistory1 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory2 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory3 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory4 = new ChallengeHistory(user2, challenge2, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+
+        challengeHistoryRepository.save(challengeHistory1);
+        challengeHistoryRepository.save(challengeHistory2);
+        challengeHistoryRepository.save(challengeHistory3);
+        challengeHistoryRepository.save(challengeHistory4);
+
+        // when
+        long participants = challengeHistoryRepository.countDistinctUserByChallengeAndChallengeStatus(challenge1, ChallengeStatus.JOIN);
+        assertThat(participants).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("상태값에 따른 유저 챌린지 이력")
+    @Order(5)
+    void user_challenge_join_list() {
+        // given
+        ChallengeHistory challengeHistory1 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.CANCEL);
+        ChallengeHistory challengeHistory2 = new ChallengeHistory(user1, challenge1, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory3 = new ChallengeHistory(user1, challenge2, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+        ChallengeHistory challengeHistory4 = new ChallengeHistory(user2, challenge2, LocalDateTime.now(), LocalDateTime.now().plusDays(7), ChallengeStatus.JOIN);
+
+        challengeHistoryRepository.save(challengeHistory1);
+        challengeHistoryRepository.save(challengeHistory2);
+        challengeHistoryRepository.save(challengeHistory3);
+        challengeHistoryRepository.save(challengeHistory4);
+
+        // when
+        List<ChallengeHistory> joinChallengeList = challengeHistoryRepository.findAllByUserAndChallengeStatus(user1, ChallengeStatus.CANCEL);
+
+        //then
+        assertThat(joinChallengeList.size()).isEqualTo(1);
+    }
+
 }
