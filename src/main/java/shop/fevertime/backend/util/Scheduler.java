a83640d2cad76d2c947c10f56bfac0f0aac7709d
jackson <steady8@kakao.com>
2021-12-14 23:20:22 +0900

    fix: new throw ApiRequestException 제거

--- src/main/java/shop/fevertime/backend/util/Scheduler.java
+++ src/main/java/shop/fevertime/backend/util/Scheduler.java
@@ -25,7 +25,6 @@ public class Scheduler {
             if (LocalDate.now().isAfter(challenge.getEndDate().toLocalDate())) {
                 challenge.updateChallengeProcess(ChallengeProgress.STOP);
             }
-            throw new ApiRequestException("asdf");
         }
     }
 }
