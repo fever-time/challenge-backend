6a5647c55ca560b0e3626fa86f56dad11a2757f9
jackson <steady8@kakao.com>
2021-12-16 22:46:19 +0900

    fix: 3000 port 추가

--- src/main/java/shop/fevertime/backend/config/WebConfig.java
+++ src/main/java/shop/fevertime/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342", "https://www.fevertime.shop")
+                .allowedOrigins("http://localhost:63342", "https://www.fevertime.shop", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
