b7cc7ee0565e2d283b78945ff8e1c81272fda732
gusrb8925 <gusrb8925@naver.com>
2021-12-17 18:18:59 +0900

    feat :  대댓글 도메인 설계 (#60)

--- src/main/java/shop/fevertime/backend/domain/Comment.java
+++ src/main/java/shop/fevertime/backend/domain/Comment.java
@@ -3,11 +3,11 @@ package shop.fevertime.backend.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import shop.fevertime.backend.dto.request.CommentRequestDto;
-import shop.fevertime.backend.exception.ApiRequestException;
 import shop.fevertime.backend.util.CommentValidator;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,6 +29,23 @@ public class Comment extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
     private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> child = new ArrayList<>();
+
+    // 연관관계 메서드
+    public void addChildComment(Comment child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+    private void setParent(Comment parent) {
+        this.parent = parent;
+    }
 
     // 댓글 생성자
     public Comment(Feed feed, String contents, User user) {
