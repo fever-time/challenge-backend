d557916bf7831f62e114ff1a3762428d248f6a27
suubinkim <0vbbbv0@gmail.com>
2022-01-07 10:45:34 +0900

    feat: sse 댓글 알림 (댓글 내용 포함x) (#111)

--- src/main/java/shop/fevertime/backend/api/SseApiController.java
+++ src/main/java/shop/fevertime/backend/api/SseApiController.java
@@ -1,6 +1,8 @@
 package shop.fevertime.backend.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,18 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import shop.fevertime.backend.service.NotificationService;
 
+@RequiredArgsConstructor
 @Slf4j
 @RestController
 public class SseApiController {
 
     private final NotificationService notificationService;
 
-    public SseApiController(NotificationService notificationService) {
-        this.notificationService = notificationService;
-    }
-
     @CrossOrigin
-    @GetMapping(value = "/sub/{userId}", produces = "text/event-stream")
+    @GetMapping(value = "/sub/{userId}")
     public SseEmitter subscribe(@PathVariable String userId) {
         return notificationService.subscribe(userId);
     }
